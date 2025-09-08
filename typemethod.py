b8526699a79bc213fc75cc0998a2536852785fbc
affan <mohdaffan.hacker@gmail.com>
2025-09-08 20:02:45 +0530

    to know the type of the data provided in the variable.

---
+++ typemethod.py
@@ -0,0 +1,13 @@
+# how to know the type of the data stored in a variable
+name = "John"
+age = 34
+percentage = 82.5
+section = 'B'
+status = True
+names = None
+print(type(name))  # This will print the type of the name variable
+print(type(age))   # This will print the type of the age variable
+print(type(percentage))  # This will print the type of the percentage variable
+print(type(section))  # This will print the type of the section variable
+print(type(status))  # This will print the type of the status variable
+print(type(names))  # This will print the type of the names variable
