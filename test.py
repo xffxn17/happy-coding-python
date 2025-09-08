683ddbe5b251a7c1f84e709e2fa0001fbd459e02
affan <mohdaffan.hacker@gmail.com>
2025-09-08 19:24:35 +0530

    Learned how comments are used in python

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 # write a program to print hello world
 print("Hello, World!")
+"""
 # This is a simple Python program that prints "Hello, World!" to the console.
 # You can run this code in any Python environment to see the output.
 # To run the program, save it in a file named test.py and execute it using the command: python test.py
 # Enjoy coding!
+"""
