72df1fd370c746c5d4b00874d4279c6800cecf10
affan <mohdaffan.hacker@gmail.com>
2025-09-08 19:41:47 +0530

    variable data store and printing it

---
+++ test2.py
@@ -0,0 +1,12 @@
+# How data is stored in Python in variables.
+name = "John"
+age = 34
+percentage = 82.5
+section = 'B'
+print(name)  # This will print the name variable
+print(age)  # This will print the age variable
+print(percentage)  # This will print the percentage variable
+print(section)  # This will print the section variable
+# This is a simple Python program that prints "Hello, World!" to the console.
+# You can run this code in any Python environment to see the output.
+# Enjoy coding!
