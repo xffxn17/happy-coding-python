7adf64d97facb60bffd9f310f06582c6a82014ff
affan <mohdaffan.hacker@gmail.com>
2025-09-12 18:17:49 +0530

    variables data handing

--- variable.py
+++ variable.py
@@ -6,7 +6,7 @@ print(x)
 print(y)
 x = 15  # now x value is changed to 15
 print(x)
-#so same can be used to store different values
-#but at a time a variable can store only one value
-#the place where i printed x it will print the value of x at that time
-#so first time it printed 10 and second time it printed 15
+# so same can be used to store different values
+# but at a time a variable can store only one value
+# the place where i printed x it will print the value of x at that time
+# so first time it printed 10 and second time it printed 15
---
+++ variable3.py
@@ -0,0 +1,5 @@
+# code
+x = 10
+print(x)
+y = 2
+# it is
