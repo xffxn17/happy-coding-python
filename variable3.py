85761f75b1d9d977e9bffcd4409e99ea49fba4eb
affan <mohdaffan.hacker@gmail.com>
2025-09-13 14:54:07 +0530

    function in python

--- variable3.py
+++ variable3.py
@@ -1,5 +1,5 @@
-# code
-x = 10
-print(x)
-y = 2
-# it is
+# how to check the length of a variable
+myname = "Mohd Affan"
+print(len(myname))  # this will going to give the length of the variable
+# how to check the type of a variable
+print(type(myname))  # this will going to give the type of the variable
