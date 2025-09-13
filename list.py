4aac499967c5b36ea21726162eec91695534181a
affan <mohdaffan.hacker@gmail.com>
2025-09-13 14:54:35 +0530

    list in python 1

---
+++ list.py
@@ -0,0 +1,8 @@
+nums = [25, 9, 5, 66, 52, 4, 6]
+print(nums)  # print entire list
+print(nums[0])  # print first element
+print(nums[-1])  # print last element
+print(nums[0:5])  # print elements from index 0 to 4
+print(nums[2:])  # print elements from index 2 to end
+print(nums)  # it prints entire list
+print("python")  # it will print python
